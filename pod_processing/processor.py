b61400195572c422e701398bbd24751d24e9004e
benjamin <2000beni770@gmail.com>
2025-09-07 17:36:46 +0300

    moving the renameing func from preprocess to process

--- pod_processing/processor.py
+++ pod_processing/processor.py
@@ -1,7 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+
 class Processor:
-    def create_unique_id(self):
-        pass
+    def rename_file(self, unique_id, file_path):
+        file_name, file_format = os.path.splitext(file_path)
+        directory_path = os.getenv("podcasts_dir")
+        new_name = f"{directory_path}/{unique_id}{file_format}"
+        os.rename(file_path, new_name)
+
+    ## -- the combination of the device id with the inode number
+    ## -- are uniquely identifying any file
+    def create_unique_id(self, message):
+        return f"{message["device_id"]}{message["inode"]}"
 
 
-    def process(self):
-        pass
+    def process(self, message):
+        unique_id = self.create_unique_id(message)
+        self.rename_file(unique_id, message["file_path"])
+
+
+
+
