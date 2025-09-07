bd95e7cb99bd0090e57ffbfb930d58590fa45a62
benjamin <2000beni770@gmail.com>
2025-09-07 16:47:41 +0300

    change port

--- pod_preprocessing/kafka_producer.py
+++ pod_preprocessing/kafka_producer.py
@@ -4,8 +4,8 @@ import json
 class Producer:
     def __init__(self):
         self.producer = KafkaProducer(
-             bootstrap_servers=['kafka:9093'],
-             value_serializer=lambda v: json.dumps(v).encode('utf-8')
+            bootstrap_servers=['localhost:9092'],
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
         )
 
     def produce(self, data):
