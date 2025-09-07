834e9efc533fdafecc4fa6d5018c52bf813f7f4b
benjamin <2000beni770@gmail.com>
2025-09-07 15:07:30 +0300

    add producer class

---
+++ pod_preprocessing/kafka_producer.py
@@ -0,0 +1,14 @@
+from kafka import KafkaProducer
+import json
+
+class Producer:
+    def __init__(self):
+        self.producer = KafkaProducer(
+             bootstrap_servers=['kafka:9093'],
+             value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
+
+    def produce(self, data):
+        topic_name = 'pod_file_meta_data'
+        self.producer.send(topic_name, data)
+        self.producer.flush()
