adf3ecc722270156588d9685a031deb9e295d77a
benjamin <2000beni770@gmail.com>
2025-09-08 17:12:02 +0300

    add logs and error handling

--- processing/mongo_dal.py
+++ processing/mongo_dal.py
@@ -1,3 +1,4 @@
+from gridfs.errors import FileExists
 from pymongo import MongoClient
 from gridfs import GridFS
 from shared.logger import Logger
@@ -9,16 +10,21 @@ class MongoDal:
         client = MongoClient(mongo_uri)
         self.db = client["podcasts"]
         self.logger = Logger.get_logger()
+        self.fs = GridFS(self.db)
+
 
 
     def insert_audio_file(self, file_path, unique_id):
-        fs = GridFS(self.db)
         with open(file_path, 'rb') as f:
             try:
-                file_id = fs.put(f, filename=unique_id, _id=unique_id, content_type='audio/wav')
+                file_id = self.fs.put(f, filename=unique_id, _id=unique_id, content_type='audio/wav')
                 self.logger.info(f"file uploaded with with file id: {file_id}")
+            except FileExists:
+                print(f"the file {file_id} already uploaded into mongodb")
+                self.logger.error(f"the file {file_id} already uploaded into mongodb")
             except Exception as e:
                 print(f"error occurred when trying to insert file to mongo: {e}")
+                self.logger.error(f"error occurred when trying to insert file to mongo: {e}")
 
 
 
