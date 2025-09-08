63e58fa9e40c28cf6c78e7cea760413d9c0261a0
benjamin <2000beni770@gmail.com>
2025-09-08 12:21:51 +0300

    add logger to the system

---
+++ logger.py
@@ -0,0 +1,33 @@
+import logging
+from elasticsearch import Elasticsearch
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+class Logger:
+    _logger = None
+    @classmethod
+    def get_logger(cls, name="logger",
+                    index="logs", level=logging.DEBUG):
+        if cls._logger:
+            return cls._logger
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+        if not logger.handlers:
+            load_dotenv()
+            es_host = os.getenv("elastic_uri")
+            es = Elasticsearch(es_host)
+            class ESHandler(logging.Handler):
+                def emit(self, record):
+                    try:
+                        es.index(index=index, document={
+                            "timestamp": datetime.utcnow().isoformat(),
+
+                            "level": record.levelname,
+                            "logger": record.name,
+                            "message": record.getMessage()
+                        })
+                    except Exception as e:
+                        print(f"ES log failed: {e}")
+            logger.addHandler(ESHandler())
+            logger.addHandler(logging.StreamHandler())
