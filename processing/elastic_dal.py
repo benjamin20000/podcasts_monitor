2235f8c37c1629568897ad11ff0bdc38c72cd3a5
benjamin <2000beni770@gmail.com>
2025-09-08 16:40:33 +0300

    create delete and create index functions

--- processing/elastic_dal.py
+++ processing/elastic_dal.py
@@ -1,13 +1,35 @@
 from elasticsearch import Elasticsearch
 from shared.logger import Logger
-from shared.config import elastic_uri
+from shared.config import elastic_uri, elastic_metadata_index
 
 
 class ElasticDal:
     def __init__(self):
         self.es_client = Elasticsearch(elastic_uri)
-        self.index_name = "files_metadata"
+        self.index_name = elastic_metadata_index
         self.logger = Logger.get_logger()
+        # self._create_index()
+
+
+    ## creating the index with schema in es if not exits
+    def _create_index(self):
+        if self.es_client.indices.exists(index=self.index_name):
+            return
+        mapping = {
+            "properties": {
+                "MB_size": {"type": "float"},
+                "creation_time": {"type": "date",
+                               "format": "yyyy-MM-dd HH:mm:ssXXX"},
+                "last_access_time": {"type": "date",
+                                  "format": "yyyy-MM-dd HH:mm:ssXXX"},
+                "last_modification_time": {"type": "date",
+                                     "format": "yyyy-MM-dd HH:mm:ssXXX"},
+                "original_file_path": {"type": "text"},
+                "current_file_path": {"type": "text"}
+            }
+        }
+        self.es_client.indices.create(index=self.index_name, body={"mappings": mapping}, ignore=400)
+
 
     def insert_metadata_doc(self, doc, unique_id ):
         try:
@@ -15,5 +37,10 @@ class ElasticDal:
             self.logger.info(f"new doc inserted into elastic with response: {response}")
             print(response)
         except Exception as e:
-            self.logger.errpr(f"error occurred when trying to insert doc into elastic: {e}")
+            self.logger.error(f"error occurred when trying to insert doc into elastic: {e}")
             print(f"error occurred when trying to insert doc into elastic: {e}")
+
+    def _delete_index(self):
+        self.es_client.indices.delete(index=self.index_name, ignore_unavailable=True)
+
+
