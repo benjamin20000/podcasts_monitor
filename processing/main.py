91a2d7c7845d260a21283e2e691f2a440e578e71
benjamin <2000beni770@gmail.com>
2025-09-09 14:57:29 +0300

    pass a callback func into consumer

--- processing/main.py
+++ processing/main.py
@@ -1,10 +1,12 @@
-from consumer import ConsumeMetadata
+from shared.kafka_consumer import Consumer
+from processor import Processor
+from shared.config import processing_kafka_topic
 
 
 def main():
-    consumer = ConsumeMetadata()
-    consumer.consume()
-
+    consumer = Consumer(processing_kafka_topic)
+    processor = Processor()
+    consumer.consume(processor.process)
 
 if __name__ == "__main__":
     main()
