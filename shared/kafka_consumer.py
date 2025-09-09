41f8eec399ba7acb0d844cf0a36a25e1c7e339ff
benjamin <2000beni770@gmail.com>
2025-09-09 13:00:22 +0300

    move kafka consumer class to shard folder and make it shareable

--- shared/kafka_consumer.py
+++ shared/kafka_consumer.py
@@ -1,16 +1,11 @@
 from kafka import KafkaConsumer
 import json
-from dotenv import load_dotenv
-import os
-from processor import Processor
+from shared.config import kafka_uri
 from shared.logger import Logger
 
 class ConsumeMetadata:
-    def __init__(self):
+    def __init__(self, topic_name):
         self.logger = Logger.get_logger()
-        load_dotenv()
-        kafka_uri = os.getenv("kafka_uri")
-        topic_name = 'pod_file_meta_data'
         self.consumer = KafkaConsumer(
             topic_name,
             bootstrap_servers=[kafka_uri],
@@ -21,12 +16,12 @@ class ConsumeMetadata:
             consumer_timeout_ms=5000
         )
 
-
-    def consume(self):
-        processor = Processor()
+    ## consume func get a callback_function
+    ## for handling with the new message arrives
+    def consume(self, callback_function):
         for message in self.consumer:
             self.logger.info(f"new message hase consume: {message.value}")
-            processor.process(message.value)
+            callback_function(message.value)
         self.consumer.close()
 
 
