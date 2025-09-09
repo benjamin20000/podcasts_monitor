36615e50e5b7faa7ecfe24a6167d5f4b6516b1b6
benjamin <2000beni770@gmail.com>
2025-09-09 12:35:38 +0300

    replace size field

--- shared/elastic_dal.py
+++ shared/elastic_dal.py
@@ -17,7 +17,7 @@ class ElasticDal:
             return
         mapping = {
             "properties": {
-                "MB_size": {"type": "float"},
+                "bytes_size": {"type": "integer"},
                 "creation_time": {"type": "date", "format": "yyyy-MM-dd HH:mm:ss"},
                 "last_access_time": {"type": "date", "format": "yyyy-MM-dd HH:mm:ss"},
                 "last_modification_time": {"type": "date", "format": "yyyy-MM-dd HH:mm:ss"},
@@ -29,7 +29,7 @@ class ElasticDal:
         self.es_client.indices.create(index=self.index_name, body={"mappings": mapping}, ignore=400)
 
 
-    def insert_metadata_doc(self, doc, unique_id ):
+    def insert_metadata_doc(self, doc, unique_id):
         try:
             response = self.es_client.index(index=self.index_name, id=unique_id, body=doc)
             self.logger.info(f"new doc inserted into elastic with response: {response}")
@@ -38,6 +38,7 @@ class ElasticDal:
             self.logger.error(f"error occurred when trying to insert doc into elastic: {e}")
             print(f"error occurred when trying to insert doc into elastic: {e}")
 
+
     def update_doc(self, doc_id, update_body):
         try:
             response = self.es_client.update(index=self.index_name, id=doc_id, body=update_body)
@@ -45,5 +46,11 @@ class ElasticDal:
         except Exception as e:
             self.logger.error(f"error occurred when trying to update a doc: {e}")
 
+
     def _delete_index(self):
         self.es_client.indices.delete(index=self.index_name, ignore_unavailable=True)
+
+
+
+# e = ElasticDal()
+# e._delete_index()
