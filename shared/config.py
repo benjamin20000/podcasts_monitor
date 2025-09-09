a8c29927b90caf3e450b4d52585e41bf176e1f47
benjamin <2000beni770@gmail.com>
2025-09-09 18:00:10 +0300

    make order in config

--- shared/config.py
+++ shared/config.py
@@ -2,13 +2,22 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-directory_path = os.getenv("podcasts_dir")
+
+## hosts
 elastic_uri = os.getenv("elastic_uri")
 mongo_uri = os.getenv("mongo_uri")
 kafka_uri = os.getenv("kafka_uri")
-elastic_metadata_index = "files_metadata"
-temp_folder_path = "../data/temp"
+
+## kafka topics
 processing_kafka_topic = "metadata_topic"
 stt_kafka_topic = "stt_topic"
+bds_kafka_topic = "bds_topic"
+
+# paths
+directory_path = os.getenv("podcasts_dir")
+temp_folder_path = "../data/temp"
 black_list_path = "../data/black_list.txt"
 grey_list_path = "../data/grey_list.txt"
+
+# es indexes
+elastic_metadata_index = "files_metadata"
