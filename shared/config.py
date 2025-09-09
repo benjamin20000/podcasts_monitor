8a2447cb60707db45878c9a84f6f1f8167e7e34e
benjamin <2000beni770@gmail.com>
2025-09-09 12:34:11 +0300

    add configurations

--- shared/config.py
+++ shared/config.py
@@ -5,5 +5,8 @@ load_dotenv()
 directory_path = os.getenv("podcasts_dir")
 elastic_uri = os.getenv("elastic_uri")
 mongo_uri = os.getenv("mongo_uri")
+kafka_uri = os.getenv("kafka_uri")
 elastic_metadata_index = "files_metadata"
 temp_folder_path = "../data/temp"
+processing_kafka_topic = "pod_file_meta_data"
+stt_kafka_topic = "stt_topic"
