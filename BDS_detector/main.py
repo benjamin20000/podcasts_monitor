736109fe9cfd655eafddefd343938191f9e22170
benjamin <2000beni770@gmail.com>
2025-09-10 15:29:07 +0300

    connect consumer to detector

--- BDS_detector/main.py
+++ BDS_detector/main.py
@@ -1,10 +1,13 @@
 from detector import Detector
+from shared.kafka_consumer import Consumer
+from shared.config import bds_kafka_topic
 
 
 def main():
+    kafka_consumer = Consumer(bds_kafka_topic)
     detector = Detector()
-    detector.detect()
-    pass
+    kafka_consumer.consume(detector.detect)
+
 
 if __name__ == "__main__":
     main()
