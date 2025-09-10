2986683f6eaf928d54844b844105718800ccd1d0
benjamin <2000beni770@gmail.com>
2025-09-10 17:44:34 +0300

    hide config

--- shared/config.py
+++ shared/config.py
@@ -14,10 +14,11 @@ stt_kafka_topic = "stt_topic"
 bds_kafka_topic = "bds_topic"
 
 # paths
-directory_path = os.getenv("podcasts_dir")
-temp_folder_path = "../data/temp"
-black_list_path = "../data/black_list.txt"
-grey_list_path = "../data/grey_list.txt"
+data_path = os.getenv("data_path")
+pod_path = f"{data_path}/podcasts"
+temp_folder_path = f"{data_path}/temp"
+black_list_path = f"{data_path}/black_list.txt"
+grey_list_path = f"{data_path}grey_list.txt"
 
 # es indexes
 elastic_metadata_index = "files_metadata"
