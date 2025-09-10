c71a7745e5c529f940fb719c71a3fe2411bf9349
benjamin <2000beni770@gmail.com>
2025-09-10 15:19:04 +0300

    make connection error handler

--- shared/kafka_consumer.py
+++ shared/kafka_consumer.py
@@ -1,20 +1,33 @@
 from kafka import KafkaConsumer
 import json
+
+from kafka.errors import NoBrokersAvailable
+
 from shared.config import kafka_uri
 from shared.logger import Logger
 
 class Consumer:
     def __init__(self, topic_name):
         self.logger = Logger.get_logger()
-        self.consumer = KafkaConsumer(
-            topic_name,
-            bootstrap_servers=[kafka_uri],
-            auto_offset_reset='earliest',
-            enable_auto_commit=True,
-            group_id='my_consumer_group',
-            value_deserializer=lambda x: json.loads(x.decode('utf-8')),
-            consumer_timeout_ms=5000
-        )
+        self.consumer = self.create_consumer(topic_name)
+
+
+    def create_consumer(self, topic_name):
+        try:
+            consumer = KafkaConsumer(
+                topic_name,
+                bootstrap_servers=[kafka_uri],
+                auto_offset_reset='earliest',
+                enable_auto_commit=True,
+                group_id='my_consumer_group',
+                value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+            )
+            self.logger.info(f"consumer with topic {topic_name} created")
+            return consumer
+        except NoBrokersAvailable as e:
+            self.logger.error(f"failed to connect to Kafka brokers: {e}")
+            raise NoBrokersAvailable("no kafka brokers could be reached")
+
 
     ## consume func get a callback_function
     ## for handling with the new message arrives
